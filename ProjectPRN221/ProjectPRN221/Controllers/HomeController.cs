e38b7063b94aac6e3d73581cbd6ac996a3b593d4
Tuan Anh <tuananh462001@gmail.com>
2023-03-26 15:46:32 +0700

    Earning

--- ProjectPRN221/ProjectPRN221/Controllers/HomeController.cs
+++ ProjectPRN221/ProjectPRN221/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyModel;
 using Microsoft.Extensions.Logging;
 using ProjectPRN221.Models;
 using System;
@@ -13,10 +14,26 @@ namespace ProjectPRN221.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         Project_SU23Context shopDB = new Project_SU23Context();
+        public List<Earning> lstEarning = new List<Earning>();
+        public List<Product> lstProduct = new List<Product>();
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+        }
+
+        public class Earning
+        {
+            public int Month { get; set; }
+            public int TotalOrder { get; set; }
+            public double Profit { get; set; }
+            public Earning() { }
+            public Earning(int month, int totalOrder, double profit)
+            {
+                Month = month;
+                TotalOrder = totalOrder;
+                Profit = profit;
+            }
         }
 
         public IActionResult Index()
@@ -29,9 +46,56 @@ namespace ProjectPRN221.Controllers
             {
                 profit += item.OrderTotalMoney;
             }
+
+            ViewBag.orderToday = shopDB.Orders.Where(x => string.Format("{0:dd/MM/yyyy}", x.OrderDate).Equals(string.Format("{0:dd/MM/yyyy}", DateTime.Now))).ToList();
+
+            var lst = shopDB.Products.ToList();
+            foreach (var item in lst)
+            {
+                if (item.ProductQuantity == 0)
+                {
+                    lstProduct.Add(item);
+                }
+            }
+
+            ViewBag.lstProduct = lstProduct;
+            ViewBag.orderDetail = shopDB.OrderDetails.ToList();
+            ViewBag.lst = lst;
             ViewBag.profit = profit;
             ViewBag.cate = "Dashboard";
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetEarning()
+        {
+            int count = 0;
+            double profits = 0;
+            for (int i = 1; i <= 12; i++)
+            {
+                foreach (var item in shopDB.Orders.ToList())
+                {
+                    string[] date = convert(string.Format("{0:dd/MM/yyyy}", item.OrderDate));
+                    string[] now = convert(string.Format("{0:dd/MM/yyyy}", DateTime.Now));
+                    if (int.Parse(date[1]) == i && date[2] == now[2])
+                    {
+                        count++;
+                        profits += item.OrderTotalMoney;
+                    }
+                    
+                }
+                lstEarning.Add(new Earning(i, count, profits));
+                count++;
+                profits = 0;
+            }
+            return Ok(lstEarning);
+        }
+
+        public string[] convert(string a)
+        {
+            int y = a.IndexOf(' ');
+            string[] b = a.Substring(0, y).Split('/');
+            return b;
         }
     }
 }
